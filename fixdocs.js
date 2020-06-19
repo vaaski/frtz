60028f95805f48bf3eed1cb3c67e694298e8e494
vaaski <admin@vaa.ski>
2020-06-20 00:58:42 +0200

    fix readme forever? prevent committing when readme is fucky

--- fixdocs.js
+++ fixdocs.js
@@ -1,7 +1,23 @@
+/* eslint-disable unicorn/no-process-exit */
+/* eslint-disable no-process-exit */
 const { read, write } = require("fs-jetpack")
+const { execSync } = require("child_process")
+
+const stopper = "<!-- commandsstop -->"
 
 // eslint-disable-next-line no-unused-expressions
 !(async () => {
-  const data = read("./README.md").replace(/\\/g, "/")
-  write("./README.md", data)
+  const gitStatus = execSync("git status --porcelain").toString().split("\n")
+  const unstaged = gitStatus.find(s => {
+    return s.startsWith(" ") && s.endsWith("README.md")
+  })
+  if (unstaged) return execSync(`git add README.md`)
+
+  const data = read("./README.md")
+  if (/\\/g.test(data) || data.endsWith(stopper + "\n")) {
+    const stopIndex = data.indexOf(stopper) + stopper.length + 1
+    const dataOut = data.slice(0, stopIndex).replace(/\\/g, "/")
+    write("./README.md", dataOut)
+    process.exit(1)
+  }
 })()
